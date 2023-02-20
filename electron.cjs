3a9ec66af6830d6284afe591732a8091f717361a
Stephan Kurennykh <stephan.kurennykh@booking.com>
2023-02-20 20:56:11 +0100

    packager

--- electron.cjs
+++ electron.cjs
@@ -28,7 +28,7 @@ const createWindow = () => {
   });
 
   if (process.env.NODE_ENV !== "development") {
-    mainWindow.loadFile(path.join(__dirname, "index.html"));
+    mainWindow.loadFile(path.join(__dirname, "dist", "index.html"));
   } else {
     mainWindow.loadURL("http://localhost:5000/");
   }
---
+++ forge.config.cjs
@@ -0,0 +1,57 @@
+const path = require("path");
+const fs = require("fs");
+
+module.exports = {
+  packagerConfig: {
+    icon: "./public/favicon",
+    // afterCopy: [
+    //   (buildPath, electronVersion, platform, arch, callback) => {
+    //     var dist = path.join(__dirname, "dist");
+    //     copyRecursiveSync(dist, buildPath);
+    //     callback();
+    //   },
+    // ],
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {},
+    },
+  ],
+};
+/**
+ * Look ma, it's cp -R.
+ * @param {string} src  The path to the thing to copy.
+ * @param {string} dest The path to the new copy.
+ */
+var copyRecursiveSync = function (src, dest) {
+  var exists = fs.existsSync(src);
+  var stats = exists && fs.statSync(src);
+  var isDirectory = exists && stats.isDirectory();
+  if (isDirectory) {
+    if (!fs.existsSync(dest)) {
+      fs.mkdirSync(dest);
+    }
+    fs.readdirSync(src).forEach(function (childItemName) {
+      copyRecursiveSync(
+        path.join(src, childItemName),
+        path.join(dest, childItemName)
+      );
+    });
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+};
