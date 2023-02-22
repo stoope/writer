e02eeadde5fff6830aacf68acf57ec51cf6a946d
Stephan Kurennykh <stephan.kurennykh@booking.com>
2023-02-22 17:40:02 +0100

    Remove unused code

--- forge.config.cjs
+++ forge.config.cjs
@@ -4,13 +4,6 @@ const fs = require("fs");
 module.exports = {
   packagerConfig: {
     icon: "./public/favicon",
-    // afterCopy: [
-    //   (buildPath, electronVersion, platform, arch, callback) => {
-    //     var dist = path.join(__dirname, "dist");
-    //     copyRecursiveSync(dist, buildPath);
-    //     callback();
-    //   },
-    // ],
   },
   rebuildConfig: {},
   makers: [
@@ -31,27 +24,4 @@ module.exports = {
       config: {},
     },
   ],
-};
-/**
- * Look ma, it's cp -R.
- * @param {string} src  The path to the thing to copy.
- * @param {string} dest The path to the new copy.
- */
-var copyRecursiveSync = function (src, dest) {
-  var exists = fs.existsSync(src);
-  var stats = exists && fs.statSync(src);
-  var isDirectory = exists && stats.isDirectory();
-  if (isDirectory) {
-    if (!fs.existsSync(dest)) {
-      fs.mkdirSync(dest);
-    }
-    fs.readdirSync(src).forEach(function (childItemName) {
-      copyRecursiveSync(
-        path.join(src, childItemName),
-        path.join(dest, childItemName)
-      );
-    });
-  } else {
-    fs.copyFileSync(src, dest);
-  }
 };
