f5c3bab3853950ff69010a0a18b8ee40123dfc29
Stephan Kurennykh <stephan.kurennykh@booking.com>
2023-03-02 17:22:40 +0100

    Fix: service-wroker scope

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,9 +1,7 @@
 export async function registerServiceWorker() {
   if ("serviceWorker" in navigator) {
     try {
-      await navigator.serviceWorker.register("./service-worker.js", {
-        scope: "/",
-      });
+      await navigator.serviceWorker.register("./service-worker.js");
     } catch (error) {
       console.error(`Service worker registration failed with ${error}`);
     }
